7c88f53523b892e48764570fe97f3ba75513e28f
Lenny Lee <lennylee91@gmail.com>
2019-10-19 05:38:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,5 +22,6 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms(attendees).each do 
+  assign_rooms(attendees).each do |attendee|
+    
 
