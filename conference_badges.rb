3343e3027599dfd73a4b481ed6019f6d1803b536
Lenny Lee <lennylee91@gmail.com>
2019-10-19 05:30:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."  
+end
+
+
+def batch_badge_creator(attendees)
+attendees.collect do |name|
+
+ badge_maker(name)
+end
+end
+
+
+def assign_rooms(attendees)
+  attendees.collect.with_index(1) do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+    
+  end
+end
+
+
